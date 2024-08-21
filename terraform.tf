21cb889ba1355f1e32f3ac641e4949c3291ab845
Raghav Arora <100126023+Raghavarora09@users.noreply.github.com>
2024-08-21 15:10:56 +0530

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -1,7 +1,7 @@
 provider "aws" {
+  access_key = "****"
+  secret_key = "****"
   region     = "ap-south-1"
-  access_key = "**********"   #replace with your access key
-  secret_key = "**********"   #replace with your secret key  
 }
 
 resource "aws_security_group" "allow_all_tcp" {
@@ -26,7 +26,7 @@ resource "aws_security_group" "allow_all_tcp" {
 resource "aws_instance" "puppet_master" {
   ami           = "ami-0c2af51e265bd5e0e" 
   instance_type = "t2.medium"
-  key_name      = "puppet"   # Replace with your key pair name
+  key_name      = "puppet"
   security_groups = [aws_security_group.allow_all_tcp.name]
 
   user_data = <<-EOF
@@ -35,9 +35,9 @@ resource "aws_instance" "puppet_master" {
 
                 apt-get install -y wget
 
-                PUBLIC_IP=$(curl -s http://169.254.169.254/latest/meta-data/public-ipv4)
+                PRIVATE_IP=$(curl -s http://169.254.169.254/latest/meta-data/private-ipv4)
 
-                echo "$PUBLIC_IP puppet" >> /etc/hosts
+                echo "$PRIVATE_IP puppet" >> /etc/hosts
 
                 wget https://apt.puppetlabs.com/puppet-release-bionic.deb
                 dpkg -i puppet-release-bionic.deb
@@ -60,7 +60,7 @@ resource "aws_instance" "puppet_master" {
 resource "aws_instance" "puppet_slave" {
   ami           = "ami-0c2af51e265bd5e0e" 
   instance_type = "t2.micro"
-  key_name      = "puppet"   # Replace with your key pair name
+  key_name      = "puppet"
   security_groups = [aws_security_group.allow_all_tcp.name]
 
   user_data = <<-EOF
@@ -68,8 +68,8 @@ resource "aws_instance" "puppet_slave" {
                 apt-get update
                 apt-get install -y wget
 
-                # Retrieve the Puppet Master's public IP
-                MASTER_IP=${aws_instance.puppet_master.public_ip}
+                # Retrieve the Puppet Master's private IP
+                MASTER_IP=${aws_instance.puppet_master.private_ip}
 
                 # Configure /etc/hosts with the Puppet Master's IP
                 echo "$MASTER_IP puppet" >> /etc/hosts
@@ -92,14 +92,14 @@ output "master_instance_id" {
   value = aws_instance.puppet_master.id
 }
 
-output "master_public_ip" {
-  value = aws_instance.puppet_master.public_ip
+output "master_private_ip" {
+  value = aws_instance.puppet_master.private_ip
 }
 
 output "slave_instance_id" {
   value = aws_instance.puppet_slave.id
 }
 
-output "slave_public_ip" {
-  value = aws_instance.puppet_slave.public_ip
+output "slave_private_ip" {
+  value = aws_instance.puppet_slave.private_ip
 }
